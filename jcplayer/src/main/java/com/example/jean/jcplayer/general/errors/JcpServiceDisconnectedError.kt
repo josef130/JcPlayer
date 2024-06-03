5307943003fd528adb908fd36f3d493e8f488266
JOSEF130 <algerinodz09@gmail.com>
2024-06-03 09:40:33 +0100

    Delete jcplayer directory

--- jcplayer/src/main/java/com/example/jean/jcplayer/general/errors/JcpServiceDisconnectedError.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jean.jcplayer.general.errors
-
-/**
- * @author Jean Carlos (Github: @jeancsanchez)
- * @date 01/05/18.
- * Jesus loves you.
- */
-object JcpServiceDisconnectedError : Throwable()
